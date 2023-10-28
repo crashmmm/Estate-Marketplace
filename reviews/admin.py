66b7e9aeeba3c901c6c51403314e060b5b262bb7
Ronak Das <ronakdasron@gmail.com>
2023-10-28 12:52:26 +0530

    added models to reviews app

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.review)
+class ReviewAdimin(admin.ModelAdmin):
+
+    """ Review Admin Definition """
+
+    pass
